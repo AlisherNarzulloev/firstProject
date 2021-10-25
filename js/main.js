f365326f4b13ec6c194e1027fce35a18273fe33e
Alisher <92950485+AlisherNarzulloev@users.noreply.github.com>
2021-10-25 21:12:30 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.swiper', {
+  // Optional paramelider  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.slider-button-next',
+    prevEl: '.slider-button-prev',
+  },
+
+});
